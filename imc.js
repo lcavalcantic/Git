78cef01a40dfff0f95b9471df957e38bf8103bfd
Letícia Cavalcanti <contcavalcanti29@gmail.com>
2023-10-31 12:18:18 -0300

    Create imc.js

---
+++ imc.js
@@ -0,0 +1,26 @@
+const peso = 54;
+const altura = 1.57;
+const imc = Number((peso / (altura*altura)).toFixed(2));
+
+console.log(imc);
+//true para entrar nos cases
+switch (true){
+    case imc < 17:
+        console.log("Muito abaixo do peso.")
+        break;
+    case imc >= 17 && imc <= 18.49:
+        console.log("Abaixo do peso.");
+        break;
+    case imc >= 18.5 && imc <= 24.99:
+        console.log("Peso normal.")
+        break;
+    case imc >= 25 && imc >= 29.99:
+        console.log("Acima do peso.")
+        break;
+    case imc >= 30 && imc <= 34.99:
+        console.log("Obesidade I.")
+        break;
+    case imc >= 35 && imc <= 39.99:
+        console.log("Obesidade II")
+        break;  
+}
